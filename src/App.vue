436320f4aaa32bdddb4cdc86c593d8037a2a2d03
482F <0ryonryoko0@gmail.com>
2021-12-22 18:05:27 +0900

    ウィンドウの見た目を調整

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <div class="main">
+        <titlebar />
+        <div class="content">
+          <HelloWorld />
+        </div>
+      </div>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Titlebar from './components/titlebar.vue'
 
 export default {
   name: 'App',
 
   components: {
     HelloWorld,
+    Titlebar,
   },
 
   data: () => ({
@@ -21,3 +28,31 @@ export default {
   }),
 }
 </script>
+
+<style lang="scss">
+html {
+  overflow: hidden !important;
+  .main {
+    height: 100vh;
+    width: 100vw;
+    font-family: Cica;
+    display: flex;
+    flex-direction: column;
+    .content {
+      flex-grow: 1;
+      overflow: auto;
+      &::-webkit-scrollbar-track {
+        background-color: #eee;
+      }
+      &::-webkit-scrollbar {
+        width: 10px;
+        height: 10px;
+      }
+      &::-webkit-scrollbar-thumb {
+        background-color: #ccc;
+        border-radius: 30px;
+      }
+    }
+  }
+}
+</style>
