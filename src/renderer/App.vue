7978d14f0685e678459daa53085e40a088d9726e
482F <482F.git.mail@482f.net>
2022-02-17 00:00:00 +0900

    ウィンドウの周りに透明の余白を設定してリサイズがしやすくなるように

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -4,7 +4,7 @@
       <div class="electron-main">
         <titlebar />
         <div class="electron-content">
-          <component :is="components[name]"/>
+          <component :is="components[name]" />
         </div>
       </div>
     </v-main>
@@ -36,7 +36,7 @@ export default {
     },
     name() {
       return location.href.match(/(?<=#).+?$/)[0]
-    }
+    },
   },
 }
 </script>
@@ -44,25 +44,30 @@ export default {
 <style lang="scss">
 html {
   overflow: hidden !important;
-  .electron-main {
-    height: 100vh;
-    width: 100vw;
-    font-family: Cica;
+  .v-main__wrap {
     display: flex;
-    flex-direction: column;
-    .electron-content {
-      flex-grow: 1;
-      overflow: auto;
-      &::-webkit-scrollbar-track {
-        background-color: #eee;
-      }
-      &::-webkit-scrollbar {
-        width: 10px;
-        height: 10px;
-      }
-      &::-webkit-scrollbar-thumb {
-        background-color: #ccc;
-        border-radius: 30px;
+    justify-content: center;
+    align-items: center;
+    .electron-main {
+      height: calc(100vh - 4px);
+      width: calc(100vw - 4px);
+      font-family: Cica;
+      display: flex;
+      flex-direction: column;
+      .electron-content {
+        flex-grow: 1;
+        overflow: auto;
+        &::-webkit-scrollbar-track {
+          background-color: #eee;
+        }
+        &::-webkit-scrollbar {
+          width: 10px;
+          height: 10px;
+        }
+        &::-webkit-scrollbar-thumb {
+          background-color: #ccc;
+          border-radius: 30px;
+        }
       }
     }
   }
